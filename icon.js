fa7f367a674c04405026d920cb4646a6dbde7047
ophs-devteam <92091147+ophs-devteam@users.noreply.github.com>
2021-11-21 19:29:18 +0600

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,78 @@
+const icons = {
+    Airport: {
+        icon: iconBase + "T_UI_Beacon_Airports.png",            
+    },
+    Bar: {
+        icon: iconBase + "T_UI_Beacon_Bars.png",
+    },
+    Bus: {
+        icon: iconBase + "T_UI_Beacon_BusStops.png",
+    },
+    Church: {
+        icon: iconBase + "T_UI_Beacon_Churches.png",
+    },
+    CoffeeShop: {
+        icon: iconBase + "T_UI_Beacon_CoffeeShops.png",
+    },
+    FireStation: {
+        icon: iconBase + "T_UI_Beacon_FireStations.png",
+    },
+    Groceries: {
+        icon: iconBase + "T_UI_Beacon_Groceries.png",
+    },
+    Gym: {
+        icon: iconBase + "T_UI_Beacon_Gyms.png",
+    },
+    HinduTemple: {
+        icon: iconBase + "T_UI_Beacon_HinduTemples.png",
+    },
+    Hospital: {
+        icon: iconBase + "T_UI_Beacon_Hospitals.png",
+    },
+    Institution: {
+        icon: iconBase + "T_UI_Beacon_Institutional.png",
+    },
+    JainTemple: {
+        icon: iconBase + "T_UI_Beacon_JainTemples.png",
+    },
+    Mosque: {
+        icon: iconBase + "T_UI_Beacon_Mosques.png",       
+    },
+    MovieThreather: {
+        icon: iconBase + "T_UI_Beacon_MovieTheaters.png",
+    },
+    Park: {
+        icon: iconBase + "T_UI_Beacon_Parks.png",
+    },
+    PoliceStation: {
+        icon: iconBase + "T_UI_Beacon_PoliceStations.png",
+    },
+    Restaurant: {
+        icon: iconBase + "T_UI_Beacon_Restaurants.png",
+    },
+    Education: {
+        icon: iconBase + "T_UI_Beacon_SchoolsAndColleges.png",
+    },
+    Shopping: {
+        icon: iconBase + "T_UI_Beacon_Shopping.png",
+    },
+    SikhTemple: {
+        icon: iconBase + "T_UI_Beacon_SikhTemples.png",
+    },
+    Synagogues: {
+        icon: iconBase + "T_UI_Beacon_Synagogues.png",
+    },
+    Train: {
+        icon: iconBase + "T_UI_Beacon_TrainStations.png",
+    },
+    Metro: {
+        icon: iconBase + "T_UI_Beacon_MetroStations.png",
+    },
+    Religious: {
+        icon: iconBase + "T_UI_Beacon_ReligiousPlace.png",
+    },
+    Recreation: {
+        icon: iconBase + "T_UI_Beacon_Recreation.png",
+    },
+     
+};
